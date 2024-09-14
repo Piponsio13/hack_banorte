55d47990b7fc2312d6dad410354fc7e9bf5483ba
Piponsio013 <pipe1910@live.com.mx>
2024-09-14 15:33:48 -0600

    Touchable Screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,27 @@
-import { Text, Image, View } from "react-native";
+import { Text, Image, View, TouchableHighlight } from "react-native";
 import styles from "../styles/HomeStyles"
 import Test from "../components/test"
 
+
+
 export default function Index() {
   return (
-      <>
-        <View style={styles.container}>
-            <Image source={require('../assets/images/BanorteLogo.png')} style={{ width: 300, height: 50 }}  />
-            <Text style={{ lineHeight: 30 }}></Text>
-            <Text style={styles.text}>TOCAR PARA INICIAR</Text>
-        </View>
+    <>
+      <View style={styles.container}>
+        <TouchableHighlight
+          underlayColor="#DDDDDD"
+          onPress={() => alert('Pressed!')}
+        >
+          <View >
+            <Image
+              source={require('../assets/images/BanorteLogo.png')}
+              style={{ width: 350, height: 50 }}
+            />
+              <Text style={{ lineHeight: 30 }}></Text>
+              <Text style={styles.text}>PRESIONA PARA INICIAR</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
     </>
   );
 }
