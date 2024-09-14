bccc7089430e164d5885a80814e45ef82a14b4f7
gadielo03 <gadielo2303@gmail.com>
2024-09-14 15:37:42 -0600

    prueba swipe

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,42 @@
 import { Text, Image, View } from "react-native";
 import styles from "../styles/HomeStyles"
 import Test from "../components/test"
+import { SwipeGesture } from "react-native-swipe-gesture-handler";
+import React, { useState } from 'react';
 
 export default function Index() {
+    const [text, setText] = useState('TOCAR PARA INICIAR');
+    const onSwipePerformed = (action) => {
+
+          switch(action){
+            case 'left':{
+              setText('left Swipe performed');
+              break;
+            }
+             case 'right':{
+              setText('right Swipe performed');
+              break;
+            }
+             case 'up':{
+              setText('up Swipe performed');
+              break;
+            }
+             case 'down':{
+              setText('down Swipe performed');
+              break;
+            }
+             default : {
+             setText('Undeteceted action');
+             }
+          }
+    }
+
   return (
       <>
         <View style={styles.container}>
             <Image source={require('../assets/images/BanorteLogo.png')} style={{ width: 300, height: 50 }}  />
             <Text style={{ lineHeight: 30 }}></Text>
-            <Text style={styles.text}>TOCAR PARA INICIAR</Text>
+            <Text style={styles.text}>{text}</Text>
         </View>
     </>
   );
