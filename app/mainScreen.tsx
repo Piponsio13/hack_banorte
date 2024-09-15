c9ff04560c0432f84d1db4c1c0b32278c9af5365
Piponsio013 <pipe1910@live.com.mx>
2024-09-15 00:00:36 -0600

    Tarjeta

--- app/mainScreen.tsx
+++ app/mainScreen.tsx
@@ -3,25 +3,24 @@ import { Text, Image, View, TouchableHighlight } from "react-native";
 import { Avatar, Button, Card} from 'react-native-paper';
 import BNCard from "../components/BNCard"
 import styles from "../styles/HomeStyles"
+import cardsData from "../cardData/cardData"
 
 export default function MainScreen() {
   return (
     <>
       <View style={styles.container}>
-        <View style={{ flex: 1, justifyContent: 'flex-start', width: "90%" }}>
-          <View>
-          <Text style={{ lineHeight: 50 }}></Text>
-          <Text style={{ lineHeight: 50 }}></Text>
+        <View style={{ flex: 1, justifyContent: 'flex-start', width: "90%", height: '100%' }}>
+          <View style={{height: '10%', alignItems: 'center', justifyContent: 'center'}}>
             <Image
               source={require('../assets/images/BanorteLogo.png')}
-              style={{ width: 350, height: 50 }}
+              style={{ width: 350, height: 50, alignItems: 'center', justifyContent: 'center' }}
             />
           </View>
           <BNCard
             title={'Titulo'}
             cardImage={'../assets/images/BanorteLogo.png'}
-            description={'Pablo'}
-            benefits={"Saaa"}
+            description={''}
+            benefits={""}
           />
         </View>
       </View>
