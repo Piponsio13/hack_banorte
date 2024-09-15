aa86d0d56e129e36a0154008e70abcfcc28841f0
Piponsio013 <pipe1910@live.com.mx>
2024-09-14 18:35:11 -0600

    Added Card in mainScreen

--- styles/HomeStyles.ts
+++ styles/HomeStyles.ts
@@ -3,13 +3,20 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#D20A0A',
+        backgroundColor: '#eb0029',
         justifyContent: 'center',
         alignItems: 'center',
     },
     text: {
         color: 'white',
         fontSize: 32,
+    },
+    card: {
+        flex: 1,
+        marginVertical: 40,
+        borderRadius: 15,
+        elevation: 4,
+
     },
 });
 
