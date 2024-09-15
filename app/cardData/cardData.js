e0c0147e7d5df922ecc4ed95b9c083dd5025fc31
gadielo03 <gadielo2303@gmail.com>
2024-09-14 21:32:29 -0600

    feat : Card Data and imgs :D

---
+++ app/cardData/cardData.js
@@ -0,0 +1,32 @@
+export const cardsData = [
+    {
+        "nombreTarjeta": "Tarjeta de Credito Clasica",
+        "descripcion": "La Tarjeta de Crédito Clásica te permite diferir todas tus compras con una tasa preferencial.",
+        "PathImg": '../assets/images//banorteTarjetasCredito',
+        "ventajas": [
+            "6 meses sin intereses. En tus compras durante los primeros 30 días después de activar tu tarjeta física.",
+            "Aprovecha y paga cómodamente con tu Tarjeta de Crédito Clásica cualquier compra mayor a $2,000 pesos.",
+            "A través de Banco en Línea puedes revisar tu línea de crédito, activar y bloquear tu tarjeta, asignar montos mínimos y máximos de compra, entre otros servicios."
+        ],
+    },
+    {
+        "nombreTarjeta": "Tarjeta de Credito Oro",
+        "descripcion": "La Tarjeta de Crédito Oro te ayuda a cumplir tus sueños y te brinda valiosos beneficios durante todo el año.",
+        "PathImg": '../assets/images/banorteTarjetasCredito/BanorteOro.png',
+        "ventajas": [
+            "Recibe 1.15 puntos por cada $10 pesos de compras que realices con tu Tarjeta de Crédito e intercámbialos por boletos de avión, experiencias o artículos del catálogo.",
+            "Transfiere el saldo de tus otras tarjetas bancarias y obtén una atractiva tasa de interés, además de plazos de 12, 18 y 24 meses.",
+            "Cuentas con un seguro que te reembolsa la diferencia de precio en caso de que encuentres el mismo producto (modelo y año) del mismo fabricante a un precio menor al que pagaste con la Tarjeta de Crédito Oro hasta por USD 400 al año."
+        ],
+    },
+    {
+        "nombreTarjeta": "Tarjeta de Credito Mujer Banorte",
+        "descripcion": "La Tarjeta de Crédito Mujer Banorte te acompaña en los distintos ámbitos de tu vida e impulsa tus proyectos mediante beneficios diseñados especialmente para ti.",
+        "PathImg": '../assets/images/banorteTarjetasCredito/BanorteMujer.png',
+        "ventajas": [
+            "Seguro para primer diagnóstico de cáncer (excepto cáncer de piel) con una cobertura de USD 1,500 más USD 500 para terapias.",
+            "Seguro de hospitalización con una cobertura de USD 100 por día y máximo USD 1,500 al año., 18 y 24 meses.",
+            "Cuentas con una cobertura hasta por USD 400 al año contra daño o robo de la mayoría de los productos y artículos nuevos comprados con la Tarjeta de Crédito Mujer Banorte."
+        ],
+    },
+]
